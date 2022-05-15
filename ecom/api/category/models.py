61e830201349d0f556fdbf13c32a7246b528048a
jamil <jamil.cse13@gmail.com>
2022-05-15 21:17:10 +0600

    serialized and created category api

--- ecom/api/category/models.py
+++ ecom/api/category/models.py
@@ -6,3 +6,6 @@ class Category(models.Model):
     description = models.CharField(max_length=250)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
